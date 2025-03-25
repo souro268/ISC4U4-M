f206f18a6e78036a75d1f96460dfd35a84d0d8fd
souro268 <63824018+souro268@users.noreply.github.com>
2025-03-25 12:53:52 -0400

    Create Assignment7.java

---
+++ Assignment7/Assignment7.java
@@ -0,0 +1,17 @@
+package Assignment7;
+
+public class Assignment7 {
+    
+    public static void main(String args[]){
+        
+        Constructor c1 = new Constructor(32,5,2020);
+        
+        c1.getShortDate();
+        c1.getLongDate();
+        
+        
+    
+    
+    
+    }
+}
