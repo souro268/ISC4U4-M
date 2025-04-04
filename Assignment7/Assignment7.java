61245ca1aff07d2ede1aa30e5f183cf16e4bd8b9
souro268 <63824018+souro268@users.noreply.github.com>
2025-04-03 22:27:04 -0400

    Update Assignment7.java

--- Assignment7/Assignment7.java
+++ Assignment7/Assignment7.java
@@ -1,17 +1,26 @@
+
 package Assignment7;
 
 public class Assignment7 {
-    
-    public static void main(String args[]){
-        
-        Constructor c1 = new Constructor(32,5,2020);
-        
-        c1.getShortDate();
-        c1.getLongDate();
-        
-        
-    
-    
-    
+
+
+
+    public static void main(String[] args) throws Exception 
+    {
+
+        //Here's how we might use the Date class
+        //This object represents today's day, March 20th, 2025
+        Date today = new Date(28,2,2024);
+
+        //Let's also create a default one; January 1st, 2000
+        Date defaultDate = new Date();
+
+        //Display both dates in short format
+        System.out.println(today.getShortDate());        // 20/03/2025
+        System.out.println(defaultDate.getShortDate());  // 01/01/2000
+
+        //Display both dates in long format
+        System.out.println(today.getLongDate());        // March 20, 2025
+        System.out.println(defaultDate.getLongDate());  // January 1, 2000
     }
 }
